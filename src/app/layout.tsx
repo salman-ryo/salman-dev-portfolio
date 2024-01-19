fa548b4566f23851402ace8fa45a23b3a2210e0a
Salman <salmankps2001@gmail.com>
2024-01-19 19:10:09 +0530

    Transition all removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`relative transition-all ${inter.className} bg-slate-100 dark:bg-gradient-to-tr from-black via-[#000929] to-[#2b034a] text-gray-950 dark:text-gray-300 pt-28 sm:pt-36`}>
+      <body className={`relative ${inter.className} bg-slate-100 dark:bg-gradient-to-tr from-black via-[#000929] to-[#2b034a] text-gray-950 dark:text-gray-300 pt-28 sm:pt-36`}>
 
         <div className="bg-[#ffc4fb] dark:hidden h-[21.25rem] -z-10 w-[21.25rem] sm:w-[58.75rem] fixed top-[-6rem] right-[11rem] rounded-full blur-[10rem]"></div>
         <div className="bg-[#9ebefd] dark:hidden h-[21.25rem] -z-10 w-[21.25rem] sm:w-[58.75rem] fixed top-[-1rem] left-[-35rem] md:left-[-33rem] lg:lef-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] rounded-full blur-[10rem]"></div>
